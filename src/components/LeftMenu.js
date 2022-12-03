b9aa157e6ac303451d3e13a8d941ac04673c4ad7
Vitorir <vitorireboucas@edu.unifor.br>
2022-12-03 17:52:05 -0300

    Adding menu components and images

---
+++ src/components/LeftMenu.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/LeftMenu.css'
+import { FaSpotify, FaEllipsisH } from 'react-icons/fa'
+import { BiSearchAlt } from 'react-icons/bi'
+
+function LeftMenu() {
+  return (
+    <div className='leftMenu'>
+        <div className='logoContainer'>
+            <i><FaSpotify /></i>
+            <h2>Spotify</h2>
+            <i><FaEllipsisH /></i>
+        </div>
+        <div className='searchBox'>
+            <input type="text" placeholder='Search...'/>
+            <i></i>
+        </div>
+    </div>
+  )
+}
+
+export {LeftMenu};
