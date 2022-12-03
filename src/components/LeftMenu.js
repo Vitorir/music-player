cb132a9471847c39c6dc2fa4f7c14df13936cc93
Vitorir <vitorireboucas@edu.unifor.br>
2022-12-03 18:52:43 -0300

    Bg and logo images, searchbox and input

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -7,8 +7,9 @@ function LeftMenu() {
   return (
     <div className='leftMenu'>
         <div className='logoContainer'>
+            
             <i><FaSpotify /></i>
-            <h2>Spotify</h2>
+            <h2>Betterfy</h2>
             <i><FaEllipsisH /></i>
         </div>
         <div className='searchBox'>
